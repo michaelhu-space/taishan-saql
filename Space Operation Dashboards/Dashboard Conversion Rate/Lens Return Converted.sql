b2544a7ef9d1c1b23b3cfa202a914494d7c0de99
MichaelHu <michael.hu@withinspace.com>
2020-08-31 14:46:21 +0800

    return converasion rate

--- Space Operation Dashboards/Dashboard Conversion Rate/Lens Return Converted.sql
+++ Space Operation Dashboards/Dashboard Conversion Rate/Lens Return Converted.sql
@@ -2,10 +2,10 @@ q = load "Recipe_Conversion_Rate";
 
 
 -- 3 months ago from this month
-q_expired_3_months = filter q by ( 'Average_Price__c' != 0 || 'Plan__c' != "Trial" )
-	&& date('End_Date__c_Year', 'End_Date__c_Month', 'End_Date__c_Day') in [null .. "2 month ago"];
+q_expired_3_months = filter q by ( 'Average_Price__c' != 0 && 'Plan__c' != "Trial" )
+	&& date('End_Date__c_Year', 'End_Date__c_Month', 'End_Date__c_Day') in [ .. "2 month ago"];
 
-q_expired_3_months =  group q_expired_3_months by ('Studio_.Name', 'Account.Person_Mobile_Phone__c');	
+q_expired_3_months =  group q_expired_3_months by ('Studio_.Name');	
 q_expired_3_months = foreach q_expired_3_months generate 
 		'Studio_.Name',
 		unique('Account.Person_Mobile_Phone__c') as 'unique_mobile';
@@ -16,11 +16,19 @@ q_return_converted_this_month = filter q by 'Last_Purchase_Membership__c' is not
 	&&  'Type_Of_Sale__c' in ["Return"]
 	&& 	date('Order_Create_Date__c_Year', 'Order_Create_Date__c_Month', 'Order_Create_Date__c_Day') in ["current month" .. "current month"];
 
-q_return_converted_this_month =  group q_return_converted_this_month by ('Studio_.Name', 'Account.Person_Mobile_Phone__c');
+q_return_converted_this_month =  group q_return_converted_this_month by ('Studio_.Name', 'Account.Person_Mobile_Phone__c', 'Id');
+q_return_converted_this_month = foreach q_return_converted_this_month generate 
+		'Studio_.Name',
+		'Account.Person_Mobile_Phone__c',
+		'Id',
+		unique('Account.Person_Mobile_Phone__c') as 'unique_mobile',
+		average('Order_Item_Payment_Price__c') as 'Order_Item_Payment_Price__c';
+
+q_return_converted_this_month =  group q_return_converted_this_month by ('Studio_.Name');
 q_return_converted_this_month = foreach q_return_converted_this_month generate 
 		'Studio_.Name',
 		unique('Account.Person_Mobile_Phone__c') as 'unique_mobile',
-		average('Order_Item_Payment_Price__c') as 'Order_Item_Payment_Price__c';
+		sum('Order_Item_Payment_Price__c') as 'Order_Item_Payment_Price__c';
 
 
 
