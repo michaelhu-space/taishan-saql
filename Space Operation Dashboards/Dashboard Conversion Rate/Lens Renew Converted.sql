bcb5dc9b64fefb21459fe03b3a352e1fe355f991
MichaelHu <michael.hu@withinspace.com>
2020-08-31 14:33:49 +0800

    finish renew sum

--- Space Operation Dashboards/Dashboard Conversion Rate/Lens Renew Converted.sql
+++ Space Operation Dashboards/Dashboard Conversion Rate/Lens Renew Converted.sql
@@ -18,12 +18,19 @@ q_renew_converted_this_month = filter q by 'Last_Purchase_Membership__c' is not 
 	&&  'Type_Of_Sale__c' in ["BurnedRenewCurrent", "BurnedRenewLast"]
 	&& 	date('Order_Create_Date__c_Year', 'Order_Create_Date__c_Month', 'Order_Create_Date__c_Day') in ["current month" .. "current month"];
 
-q_renew_converted_this_month =  group q_renew_converted_this_month by ('Studio_.Name', 'Account.Person_Mobile_Phone__c');
+q_renew_converted_this_month =  group q_renew_converted_this_month by ('Studio_.Name', 'Account.Person_Mobile_Phone__c', 'Id');
 q_renew_converted_this_month = foreach q_renew_converted_this_month generate 
 		'Studio_.Name',
+		'Account.Person_Mobile_Phone__c',
+		'Id',
 		unique('Account.Person_Mobile_Phone__c') as 'unique_mobile',
 		average('Order_Item_Payment_Price__c') as 'Order_Item_Payment_Price__c';
 
+q_renew_converted_this_month =  group q_renew_converted_this_month by ('Studio_.Name');
+q_renew_converted_this_month = foreach q_renew_converted_this_month generate 
+		'Studio_.Name',
+		unique('Account.Person_Mobile_Phone__c') as 'unique_mobile',
+		sum('Order_Item_Payment_Price__c')  as 'Order_Item_Payment_Price__c';
 
 
 result = cogroup 	q_burned_this_month 			by ('Studio_.Name') full, 
@@ -38,9 +45,9 @@ r_renew_converted_this_month = foreach result generate
 	
 	sum(q_burned_this_month.'unique_mobile') as 'Burned amount this month', 
 	sum(q_renew_converted_this_month.'unique_mobile') as 'Renew Converted amount this month',
-	number_to_string(sum(q_renew_converted_this_month.'unique_mobile')/sum(q_burned_this_month.'unique_mobile'), "#.00%") as 'Converted Rate',
+	number_to_string(average(q_renew_converted_this_month.'unique_mobile')/sum(q_burned_this_month.'unique_mobile'), "#.00%") as 'Converted Rate',
 
-	number_to_string(sum(q_renew_converted_this_month.'Order_Item_Payment_Price__c'), "#.00") as 'Sum of converted packages',
+	number_to_string(average(q_renew_converted_this_month.'Order_Item_Payment_Price__c'), "#.00") as 'Sum of converted packages',
 	number_to_string(sum(q_renew_converted_this_month.'Order_Item_Payment_Price__c')/sum(q_renew_converted_this_month.'unique_mobile'), "#.00") as 'price per customer';
 	;
 
