415dcb4d28e9b33b548706eb1a7087d1f95ed334
MichaelHu <michael.hu@withinspace.com>
2020-09-07 14:31:44 +0800

    pivot instructor fill rate with total

--- Space EPT Dashboards/Dashboard Instructor Fill Rate/Pivot Instructor Fill Rate.sql
+++ Space EPT Dashboards/Dashboard Instructor Fill Rate/Pivot Instructor Fill Rate.sql
@@ -1,10 +1,16 @@
 q = load "Dataset_Schedule_Fill_Rate";
 
+-- q = filter q by 
+-- date('Class_Start_Time__c_Year', 'Class_Start_Time__c_Month', 'Class_Start_Time__c_Day') in [dateRange([2020,6,01], [2020,6,30])]
+-- -- && 'J_Instr.Instructor_Name__c' == "王磊 Ray"
+-- ;
+
 -- Primary instructor
 q_primary_instructor = filter q by 
 		'Status__c' == "Published"
 		&& 'J_Instr.Is_Active__c' == "true"
 		&& 'J_Instr.Instructor_Role__c' == "Primary Instructor"
+		-- 
 ;
 q_primary_instructor = foreach q_primary_instructor generate 
 		'J_Instr.Instructor_Name__c' as 'Primary Instructor',
@@ -12,8 +18,7 @@ q_primary_instructor = foreach q_primary_instructor generate
 		"" as 'Assistant Instructor',
 		'J_Instr.Instructor_Role__c',
 		'Modalit.Name',
-		unique('Id') as 'Id';
-
+		unique('Id') as 'unique_Id';
 
 
 -- Substitute instructor
@@ -28,7 +33,7 @@ q_substitute_instructor = foreach q_substitute_instructor generate
 		"" as 'Assistant Instructor',
 		'J_Instr.Instructor_Role__c',
 		'Modalit.Name',
-		unique('Id') as 'Id';
+		unique('Id') as 'unique_Id';
 
 
 -- Assistant instructor
@@ -43,8 +48,7 @@ q_assistant_instructor = foreach q_assistant_instructor generate
 		'J_Instr.Instructor_Name__c' as 'Assistant Instructor',
 		'J_Instr.Instructor_Role__c',
 		'Modalit.Name',
-		unique('Id') as 'Id';
-
+		unique('Id') as 'unique_Id';
 
 
 
@@ -56,7 +60,7 @@ q_instructor_fill_rate = cogroup 	q_primary_instructor 		by ('Primary Instructor
 				                	;
 
 
-r_instructor_fill_rate = foreach q_instructor_fill_rate generate 
+q_instructor_fill_rate = foreach q_instructor_fill_rate generate 
 				
 				coalesce(
 					q_primary_instructor.'Primary Instructor',
@@ -84,13 +88,26 @@ r_instructor_fill_rate = foreach q_instructor_fill_rate generate
 				) as 'Modalit.Name',
 
 				coalesce(
-					sum(q_primary_instructor.'Id'), 
-					sum(q_substitute_instructor.'Id'), 
-					sum(q_assistant_instructor.'Id')
-				) as 'Instructor Attendance Count'
+					sum(q_primary_instructor.'unique_Id'), 
+					sum(q_substitute_instructor.'unique_Id'), 
+					sum(q_assistant_instructor.'unique_Id')
+				) as 'Attendance Count'
 				;
 
-			
+q_instructor_fill_rate = group q_instructor_fill_rate by rollup('Primary Instructor','Substitute Instructor','Assistant Instructor','Modalit.Name');
+
+r_instructor_fill_rate = foreach q_instructor_fill_rate generate 
+		(case
+	        when grouping('Primary Instructor') == 1 then "～ Total"
+	        else 'Primary Instructor'
+	    end) as 'Primary Instructor',
+		'Substitute Instructor',
+		'Assistant Instructor',
+		(case
+	        when grouping('Modalit.Name') == 1 then "～ Total"
+	        else 'Modalit.Name'
+	    end) as 'Modalit.Name',
+		sum('Attendance Count') as 'Attendance Count';
 		
 r_instructor_fill_rate = order r_instructor_fill_rate by ( 'Primary Instructor' asc, 'Substitute Instructor' asc);
 r_instructor_fill_rate = limit r_instructor_fill_rate 10000;
